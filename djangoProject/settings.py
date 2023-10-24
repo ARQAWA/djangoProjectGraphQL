418f1b6b913e3755d1dee6b5b22bd70de0b1bf89
Аркадий <chuark@gmail.com>
2023-10-25 00:37:19 +0300

    добавил zorgs

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "test_django_graphql",
     "app2",
+    "zorgs",
 ]
 
 MIDDLEWARE = [
---
+++ zorgs/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class MegaZorg(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "app3_megazorg"
