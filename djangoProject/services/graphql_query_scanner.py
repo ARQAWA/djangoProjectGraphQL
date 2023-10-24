66eae1e1be66964725cdbf11532c6b8db8bbadf7
Аркадий <chuark@gmail.com>
2023-10-25 02:10:24 +0300

    генератор схемы первого уровня

---
+++ djangoProject/services/graphql_query_scanner.py
@@ -0,0 +1,26 @@
+from django.apps import apps
+
+
+class GraphQLQueryFabric:
+    """This class is responsible for generating GraphQL queries from Django models"""
+
+    def __init__(self):
+        self._apps_and_models = self._construct_apps_and_models()
+
+    @staticmethod
+    def _construct_apps_and_models() -> dict[str, set[str]]:
+        apps_and_models: dict[str, set[str]] = {}
+
+        for app_config in apps.get_app_configs():
+            if app_config.name.startswith("django.contrib"):
+                continue
+
+            model_names = set()
+            for model in app_config.get_models():
+                model_name = getattr(getattr(model, "_meta"), "object_name")
+                model_names.add(model_name)
+
+            print(app_config.name, model_names)
+            apps_and_models[app_config.name] = model_names
+
+        return apps_and_models
